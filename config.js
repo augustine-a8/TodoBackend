da09e653bda2396c99e668eb6d57de3fe607c134
augustine-a8 <logos.agbeko@gmail.com>
2022-07-06 09:37:13 +0000

    config file added

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+  LOCALMONGODB:
+    "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000",
+  MONGODBURL:
+    "mongodb+srv://augustinea:${Augustine}@cluster0.lrox5.mongodb.net/?retryWrites=true&w=majority",
+};
+
+export default config;
